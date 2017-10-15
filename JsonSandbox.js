328da397b854f6bcb85e30400f5a98f33795f863
Orson Badger <badgero1234@byui.edu>
2017-10-14 21:09:47 -0600

    added a jsonSandBox to the code.

---
+++ JsonSandbox.js
@@ -0,0 +1,70 @@
+function jsonBoxOSand(){ //My JSON Sanbox to play in 
+
+
+    //What is Json? Json is essentially the way to make an object
+    //for javascript. It has key value pairs.
+    //Rules
+    //Data is in name/value pairs
+    //Curly braaces hold objects 
+
+    //Happy Path
+    var jsonStringExample = {"friends": [
+                            {"name": "David", "surname":"Truscott"},
+                            {"name": "Jake", "surname": "Robertson"},
+                            {"name": "Samantha", "surname" : "Stavist"}                        
+                            ]}
+
+    console.log(jsonStringExample);
+    console.log(JSON.parse(jsonStringExample)); //parse out the json to a javascript Object
+
+
+    //JSON.parse cannot parse a non json object.
+
+    try { //Use a try catch to not break the function. This allows it to keep running.
+        console.log(JSON.parse("Hello World I am a monkey"));
+    }catch(e){
+        console.log(e.toString()); //Make the error a string to see what it is
+    }
+
+    //Is it possible to pass a null?
+    console.log(JSON.parse(null)); //This will not throw an exception but
+                                   //it will just print "null"
+
+    //Nasty Path
+    var badjsonExample = {"friends": [
+        {"name": "David", "surname":"Truscott"},
+        {"name": "Jake", "surname": "Robertson"},
+        {"name": "Samantha", "surname" : "Stavist"},                        
+        }; //Leave out the closing brace and you are doomed...Not really just it will break
+    
+    try{
+        console.log(JSON.parse(badjsonExample));
+    }catch(e)    {
+        console.log(e.toString);
+    }
+
+    /////////Implementation of Stringify/////////
+
+    var jsonObj = {"friends": [
+        {"name": "David", "surname":"Truscott"},
+        {"name": "Jake", "surname": "Robertson"},
+        {"name": "Samantha", "surname" : "Stavist"}                        
+        ]}
+
+    console.log(jsonObj);
+
+    console.log(JSON.stringify(jsonObj));
+
+    //Nasty Paths
+
+    // Obviously you cannot do stringify something if it is null.
+    console.log(JSON.stringify(null));
+
+    //it wont do anything if you pass a string either
+    console.log(JSON.stringify("Strings dont do much...."));
+
+    //another thing is it does nothing if it is just
+    var nothingness;
+    console.log(nothingness);
+
+}
